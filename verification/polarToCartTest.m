34589278829662baca9942b0d0a8161bef198eed
Fer Clerkx <f.clerkx@student.fontys.nl>
2023-11-08 16:15:20 +0100

    Added polarToCart test boilerplate.

---
+++ verification/polarToCartTest.m
@@ -0,0 +1,10 @@
+function tests = polarToCartTest
+    tests = functiontests(localfunctions);
+end
+
+% example test
+% function testRealSolution(testCase)
+%     actSolution = quadraticSolver(1,-3,2);
+%     expSolution = [2 1];
+%     verifyEqual(testCase,actSolution,expSolution)
+% end
