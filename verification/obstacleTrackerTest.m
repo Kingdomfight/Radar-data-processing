2deddaf728cb9e6a5df4007348391241c38d81a1
Fer Clerkx <f.clerkx@student.fontys.nl>
2023-11-14 15:14:50 +0100

    Added obstacleTracker test "testNotDetectedActive".

--- verification/obstacleTrackerTest.m
+++ verification/obstacleTrackerTest.m
@@ -38,3 +38,24 @@ function testDetectedActive(testCase)
 	expOutput{3} = {x(3), y(3), vX(2), vY(2), true};
 	verifyEqual(testCase, actOutput, expOutput)
 end
+
+function testNotDetectedActive(testCase)
+	dt = 0.25;
+	vX = -7;
+	vY = 4;
+	x(1) = -3; y(1) = -2;
+
+	x(2) = x(1)+vX*dt;
+	y(2) = y(1)+vY*dt;
+	x(3) = x(2)+vX*dt;
+	y(3) = y(2)+vY*dt;
+
+	[actOutput{1}{1:5}] = testCase.TestData.dut(0, x(1), y(1), true);
+	[actOutput{2}{1:5}] = testCase.TestData.dut(dt, x(2), y(2), true);
+	[actOutput{3}{1:5}] = testCase.TestData.dut(dt*2, 0, 0, false);
+
+	expOutput{1} = {x(1), y(1), 0, 0, true};
+	expOutput{2} = {x(2), y(2), vX, vY, true};
+	expOutput{3} = {x(3), y(3), vX, vY, true};
+	verifyEqual(testCase, actOutput, expOutput)
+end
