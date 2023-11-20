be1bb37e415a3a6d147c156dca7ca280d0c15237
Fer Clerkx <Fer@DESKTOP-U5DRS87>
2023-11-20 21:48:23 +0100

    Updated zoneCheck input and constant names. Added description.

--- algorithm/zoneCheck.m
+++ algorithm/zoneCheck.m
@@ -1,5 +1,16 @@
 classdef zoneCheck < matlab.System
-	% Checks if any obstacles o come within a 5 meter radius of the radar.
+	% Checks if any obstacles in array obsState enter a RADIUS meter radius
+	% around the radar in the next TIME_THRESHOLD seconds. Only obstacles
+	% actively tracking are checked.
+	% Input obsState is an array of structs containing the state variables
+	% of obstacleTracker objects. These include:
+	%   px: x coordinate
+	%   py: y coordinate
+	%   vx: x velocity
+	%   vy: y velocity
+	%   tracking: obstacleTracker object is tracking object(true) or not(false)
+	% Output warning is true if an obstacleTracker object will enter a RADIUS
+	% meter radius around the radar in the next TIM_THRESHOLD seconds.
 
 	% Public, tunable properties
 	properties
@@ -12,8 +23,12 @@ classdef zoneCheck < matlab.System
 
 	% Pre-computed constants
 	properties (Access = private)
-		radius = 5;
-		timeThreshold = 5;
+
+	end
+
+	properties (Constant = true)
+		RADIUS = 5;
+		TIME_THRESHOLD = 5;
 	end
 
 	methods (Access = protected)
@@ -21,21 +36,21 @@ classdef zoneCheck < matlab.System
 			% Perform one-time calculations, such as computing constants
 		end
 
-		function warning = stepImpl(obj,o)
+		function warning = stepImpl(obj, obsState)
 			% Represent obstacle state vector as a line: y=ax+b
-			a = o.Vy./o.Vx;
-			b = o.Py - o.Px.*a;
+			a = obsState.vy./obsState.vx;
+			b = obsState.py - obsState.px.*a;
 			% Calculate discriminant system of equations of line and 
-			% circle x^2+y^2=radius^2
-			D = 4*((a*obj.radius).^2 - b.^2 + obj.radius^2);
+			% circle x^2+y^2=RADIUS^2
+			D = 4*((a*obj.RADIUS).^2 - b.^2 + obj.RADIUS^2);
 
 			x1 = (-2*a.*b + sqrt(D))./(2*a.^2 + 2);
 			x2 = (-2*a.*b - sqrt(D))./(2*a.^2 + 2);
-			t1 = (x1 - o.Px)/o.Vx;
-			t2 = (x2 - o.Px)/o.Vx;
+			t1 = (x1 - obsState.px)/obsState.vx;
+			t2 = (x2 - obsState.px)/obsState.vx;
 
-			warning = any((isreal(t1) && t1 < obj.timeThreshold && t1 >= 0) ...
-			| (isreal(t2) && t2 < obj.timeThreshold && t2 >= 0));
+			warning = any((isreal(t1) && t1 < obj.TIME_THRESHOLD && t1 >= 0) ...
+			| (isreal(t2) && t2 < obj.TIME_THRESHOLD && t2 >= 0));
 		end
 
 		function resetImpl(~)
