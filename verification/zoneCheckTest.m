e2addc6ec1497732c1e8c9b64a3bd0a7c3dc93b5
Fer Clerkx <Fer@DESKTOP-U5DRS87>
2023-11-20 22:53:12 +0100

    Added zoneCheck test for moving into zone.

--- verification/zoneCheckTest.m
+++ verification/zoneCheckTest.m
@@ -7,15 +7,33 @@ function setup(testCase)
 end
 
 function testObstacleInZone(testCase)
-	input = struct('px', 0, 'py', 0, 'vx', 0, 'vy', 0, 'tracking', 1);
+	input = struct('px', 0, 'py', 0, 'vx', 0, 'vy', 0, 'tracking', true);
 	actOut = testCase.TestData.dut(input);
-	expOut = true;
-	verifyEqual(testCase, actOut, expOut);
+	verifyEqual(testCase, actOut, true);
 end
 
 function testObstacleOnBorder(testCase)
-	input = struct('px', 5, 'py', 0, 'vx', 0, 'vy', 0, 'tracking', 1);
+	input = struct('px', testCase.TestData.dut.RADIUS, 'py', 0, 'vx', 0, 'vy', 0, 'tracking', true);
 	actOut = testCase.TestData.dut(input);
-	expOut = true;
-	verifyEqual(testCase, actOut, expOut);
+	verifyEqual(testCase, actOut, true);
+end
+
+function testObstacleMovingDiagonallyIntoZone(testCase)
+	px = -testCase.TestData.dut.RADIUS;
+	py = -testCase.TestData.dut.RADIUS;
+	vx = -px/testCase.TestData.dut.TIME_THRESHOLD;
+	vy = -py/testCase.TestData.dut.TIME_THRESHOLD;
+	input = struct('px', px, 'py', py, 'vx', vx, 'vy', vy, 'tracking', true);
+	actOut = testCase.TestData.dut(input);
+	verifyEqual(testCase, actOut, true);
+end
+
+function testObstacleMovingVerticallyIntoZone(testCase)
+	px = 0;
+	py = -testCase.TestData.dut.RADIUS;
+	vx = 0;
+	vy = -py/testCase.TestData.dut.TIME_THRESHOLD;
+	input = struct('px', px, 'py', py, 'vx', vx, 'vy', vy, 'tracking', true);
+	actOut = testCase.TestData.dut(input);
+	verifyEqual(testCase, actOut, true);
 end
